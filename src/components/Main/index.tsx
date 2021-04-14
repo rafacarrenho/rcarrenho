8533c47a11ea8105182f65c7fc080e489f4c9344
rafacarrenho <rafael.carrenho@hotmail.com>
2021-04-14 09:31:00 -0300

    first commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,43 @@
-import * as S from './styles'
+import * as S from "./styles"
+import { FaGithub, FaLinkedinIn, FaBehance } from "react-icons/fa"
+import { IoIosMail } from "react-icons/io"
 
-const Main = ({
-  title = 'React Avançado',
-  description = 'TypeScript, ReactJS, NextJS e Styled Components'
-}) => (
+const Main = () => (
   <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
-    />
-    <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela com código."
-    />
+    <S.Title>
+      Desenvolvedor, Especialista em Front End, ReactJS, NextJS, Comunicativo,
+      Organizado
+    </S.Title>
+    <S.Links>
+      <S.Line />
+      <a href="https://stackoverflow.com/">
+        <FaBehance />
+      </a>
+      <a href="https://www.linkedin.com/in/rafael-barbosa-carrenho-a98b3254/">
+        <FaLinkedinIn />
+      </a>
+      <a href="https://github.com/rafacarrenho">
+        <FaGithub />
+      </a>
+      <a href="mailto:rafael.carrenho@hotmail.com">
+        <IoIosMail />
+      </a>
+    </S.Links>
+    <S.Description>
+      <p>
+        Olá, sou o <strong>Rafael Carrenho</strong>, desenvolvedor{" "}
+        <strong>Front End</strong> especialista em <strong>JavaScript</strong>,
+        em especial com <strong>ReactJS</strong> e <strong>NextJS</strong> com
+        mais de <strong>2 anos de experiencias</strong> no mesmos e{" "}
+        <strong>8 anos de experiencia</strong> na area de{" "}
+        <strong>tecnologia</strong>.
+      </p>{" "}
+      <p>
+        Entre minhas habilidades que me permitem ser um profissional
+        extremamente qualificado estão a ótima comunicação, organização, atenção
+        aos detalhes, além de vontade e empenho para sempre estar melhorando.
+      </p>
+    </S.Description>
   </S.Wrapper>
 )
 
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,33 +1,31 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
 export const Wrapper = styled.main`
-  background-color: #06092b;
-  color: #fff;
   width: 100%;
   height: 100%;
   padding: 3rem;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
-
-export const Logo = styled.img`
-  width: 25rem;
-  margin-bottom: 2rem;
 `
 
 export const Title = styled.h1`
-  font-size: 2.5rem;
+  text-align: left;
+  font-size: 6rem;
 `
 
 export const Description = styled.h2`
   font-size: 2rem;
   font-weight: 400;
+  margin-left: 7rem;
 `
 
-export const Illustration = styled.img`
-  margin-top: 3rem;
-  width: min(30rem, 100%);
+export const Links = styled.div`
+  display: flex;
+  font-size: 2.5rem;
+  gap: 2rem;
+  align-items: center;
+`
+
+export const Line = styled.div`
+  width: 16rem;
+  height: 0.5rem;
+  background-color: black;
 `
