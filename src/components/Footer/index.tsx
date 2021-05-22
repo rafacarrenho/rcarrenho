52d4faebcf02b533c71315c331a5d4dc52ed92a6
rafacarrenho <rafael.carrenho@hotmail.com>
2021-05-22 19:00:35 -0300

    Fix: Remove container from footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,4 @@
 import Social from "components/Social"
-import { Container } from "next/app"
 import React from "react"
 import { HiCode } from "react-icons/hi"
 import * as S from "./styles"
@@ -11,14 +10,12 @@ type Props = {
 const Footer = ({ footerText }: Props) => {
   return (
     <S.Wrapper>
-      <Container>
-        <S.IconContainer>
-          <HiCode />
-        </S.IconContainer>
-        <span dangerouslySetInnerHTML={{ __html: footerText }} />
-        <hr />
-        <Social />
-      </Container>
+      <S.IconContainer>
+        <HiCode />
+      </S.IconContainer>
+      <span dangerouslySetInnerHTML={{ __html: footerText }} />
+      <hr />
+      <Social />
     </S.Wrapper>
   )
 }
