3b861ce0d90dfe13101f4eecd236d43d9fb5b25b
rafacarrenho <rafael.carrenho@hotmail.com>
2021-05-24 13:49:25 -0300

    Feat: Title and description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,13 +9,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={darkTheme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Rafael Carrenho</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <meta name="theme-color" content="#06092B" />
         <link rel="shortcut icon" href="img/favicon.svg" type="image/svg" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Rafael Carrenho desenvolvedor Front End - Site portfólio"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
