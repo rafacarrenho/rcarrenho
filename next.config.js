0bff5d57c7507a470b89f5768ecba5f045404567
Rafael Carrenho <rafael.carrenho@hotmail.com>
2022-12-31 16:44:47 -0300

    feat: upgrade to next 13

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require("next-pwa")
-const isProd = process.env.NODE_ENV === "production"
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    disable: !isProd
-  },
-  future: {
-    webpack5: true
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ["media.graphcms.com", "media.graphassets.com"]
+  },
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true
+    }
   }
-})
+}
+
+module.exports = nextConfig
